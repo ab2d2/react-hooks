62234ab6d9ea6494f97e92a019feb9976c3bea05
ab2d2 <abdul.rahimi@capgemini.com>
2020-03-23 22:49:53 +0000

    useRef example problem

---
+++ useRef.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Counter = ({ max, step }) => {
+  const [count, setCount] = useState(0);
+  const countRef = React.useRef();
+
+  let message = '';
+  if (countRef.current < count) message = 'Higher';
+  if (countRef.current > count) message = 'Lower';
+
+  countRef.current = count;
+
+  const increment = () => {
+    setCount(c => c + 1);
+  };
+
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`Count: ${count}`);
+    }, 3000);
+  }, [count]);
+
+  return (
+    <div className='Counter'>
+      <p>{message}</p>
+      <p className='count'>{count}</p>
+      <section className='controls'>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
+};
